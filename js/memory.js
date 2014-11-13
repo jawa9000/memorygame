dd5a49e5fb5f4bbcaa25df1935d5ea0c257b7b0b
jawa9000 <jawa9000@gmail.com>
2014-11-13 08:45:37 -0800

    made a breakthrough on random icon generation

--- js/memory.js
+++ js/memory.js
@@ -1,7 +1,151 @@
-//var icons = ["asterisk","cloud","music","heart","star","user","home","file","lock","headphones","volume-up","book","camera","picture","tint","play","arrow-right","fire","eye-open","thumbs-up","globe","phone","floppy-disk","tower","tree-deciduous"]; // all possible icons
-var icons = ["cloud","cloud","heart","heart","star","star","home","home","lock","lock","volume-up","volume-up","camera","camera","tint","tint","fire","fire","eye-open","eye-open","globe","globe","floppy-disk","floppy-disk","tree-deciduous"]; // matching icons with one odd
+var icons = ["asterisk","cloud","music","heart","star","user","home","file","lock","headphones","volume-up","book","camera","picture","tint","play","arrow-right","fire","eye-open","thumbs-up","globe","phone","floppy-disk","tower","tree-deciduous"]; // all possible icons
+//var icons = ["cloud","cloud","heart","heart","star","star","home","home","lock","lock","volume-up","volume-up","camera","camera","tint","tint","fire","fire","eye-open","eye-open","globe","globe","floppy-disk","floppy-disk","tree-deciduous"]; // matching icons with one odd
 var defaultIcon = "question-sign";
 
+// Create buttons to select field size
+var fields = ["2x2","3x3","4x4","5x5","6x6"];
+var fieldMessage = "<div class='table'><div class='row'>";
+for (var i = 0; i < fields.length; i++) {
+	fieldMessage += "<div data='" + (i+2) + "' class='button' id='field" + i + "'>" + fields[i] + "</div>";
+}
+fieldMessage += "</div></div>";
+$("#buttons").html(fieldMessage);
+
+// Display field based on button clicked
+$(".button").click(function() {
+	var buttonValue = $(this).attr("data");
+	/*
+	var fieldSize = buttonValue * buttonValue;
+	var iconSet;
+	switch(parseInt(buttonValue)) { // based on the value of buttonValue, create a number that will be used to pair icons
+		case 2:
+			iconSet = 2; //2
+			break;
+		case 3:
+			iconSet = Math.floor(Math.random() * 2) + 3; //3-4 icon sets
+			break;
+		case 4:
+			iconSet = Math.floor(Math.random() * 5) + 4; //4-8 icon sets
+			break;
+		case 5:
+			iconSet = Math.floor(Math.random() * 8) + 5; // 5-12 icon sets
+			break;
+		case 6:
+			iconSet = Math.floor(Math.random() * 13) + 6; // 6-18 icons sets
+			break;
+	}
+	*/
+	// ** figure out what the difference is between buttonValue and iconSet
+	// ** generate a list of icons based on the value of iconSet
+	//console.log("iconSet: " + iconSet);
+	//console.log("fields: " + (buttonValue * buttonValue));
+	
+	
+	var randomTiles = [];
+	
+	/*
+	// what if i used a 2d array; the size of the 2d array would depend on buttonValue
+	var finalArray = [];
+	
+	for (var i = 0; i < buttonValue; i++) {
+		var tempArray = [];
+		for (var j = 0; j < buttonValue; j++) {
+			var rndIcon = Math.floor(Math.random() * icons.length);
+			tempArray.push(icons[rndIcon]);
+		}
+		finalArray.push(tempArray);
+	}
+	console.log(finalArray);
+	*/
+	
+	// loop through buttonValue * buttonValue / 2
+	// randomly pick an icon
+	// push that random icon twice
+	var loop = (buttonValue * buttonValue) / 2;
+	// if (loop % 2) ....
+	for (var i = 0; i < loop; i++) {
+		var rndIcon = Math.floor(Math.random() * icons.length);
+		randomTiles.push(icons[rndIcon]);
+		randomTiles.push(icons[rndIcon]);
+	}
+	
+	
+	/*
+	// based on buttonValue, generate a list of icons
+	for (var i = 0; i < buttonValue; i++) {
+		var rndIcon = Math.floor(Math.random() * icons.length);
+		randomTiles.push(icons[rndIcon]);
+	}
+	*/
+	
+	/*
+	// multiple the size of the randomTiles array by itself to get the correct number of icons
+	var myLength = randomTiles.length;
+	myLength = myLength * myLength;
+	//console.log(myLength);
+	for (var i = 0; i < myLength - 1; i++) {
+		randomTiles.push(randomTiles[i]);
+	}
+	*/
+	console.log(randomTiles);
+	
+	// randomize order of randomTiles
+	var tempArray = []; // array to hold a copy of randomTiles
+	var randomArray = []; // array to hold randomly ordered version of randomTiles
+	for (i in randomTiles) { // copy randomTiles to tempArray
+	 	tempArray.push(randomTiles[i]);
+	}
+	for (var i = 0; i < randomTiles.length; i++) {
+		if (tempArray.length > 0) {
+			var rnd = Math.floor(Math.random() * (tempArray.length - 1));
+			randomArray.push(tempArray[rnd]);
+			tempArray.splice(rnd, 1);
+	 	}
+	}
+	//console.log("original order (randomTiles): " + randomTiles);
+	//console.log("final order (randomArray): " + randomArray);
+	
+	// build field with icons
+	var tempCount = 0;
+	var field = "<div class='table'>";
+	for (var i = 0; i < buttonValue; i++) {
+	 	field += "<div class='row' id=fieldRow" + i + ">";
+	 	for (var j = 0; j < buttonValue; j++) {
+	 		//console.log(randomArray[tempCount]);
+	 		//field += "<div data='" + randomTiles[tempCount] + "' class='cell glyphicon glyphicon-" + defaultIcon + "' id='fieldCell" + i + "-" + j + "'></div>";
+	 		field += "<div data='" + randomTiles[tempCount] + "' class='cell glyphicon glyphicon-" + randomArray[tempCount] + "' id='fieldCell" + i + "-" + j + "'></div>";
+	 		tempCount++;
+	 	}
+	 	field += "</div>";
+	}
+	field += "</div>";
+	$("#field").html(field);
+});
+
+
+/*
+ * $(".button").click(function() {
+
+	randomizeArray(iconSet,randomTiles);
+	
+	// generate field based on button click
+	var tempCount = 0;
+	var field = "<div class='table'>";
+	for (var i = 0; i < iconSet; i++) {
+	 	field += "<div class='row' id=fieldRow" + i + ">";
+	 	for (var j = 0; j < iconSet; j++) {
+	 		field += "<div class='cell' id='fieldCell" + i + "-" + j + "'>" + randomTiles[tempCount] + "</div>";
+	 		tempCount++;
+	 	}
+	 	field += "</div>";
+	}
+	field += "</div>";
+	$("#field").html(field);	
+	// end field generation 
+});
+ */
+
+/*
 var count = 0; // generic number to iterate through icon array when drawing the board
 var boardSize = 5; // number of rows and columns to create for the board
 var clicks = 0; // count the number of times the user clicks
@@ -62,43 +206,9 @@ $(document).ready(function() {
 
 // fields and ramdonly picked cell values
 
-var fields = ["2x2","3x3","4x4","5x5","6x6"];
-var fieldMessage = "<div class='table'><div class='row'>";
-for (var i = 0; i < fields.length; i++) {
-	fieldMessage += "<div class='button' id='field" + i + "'>" + fields[i] + "</div>";
-}
-fieldMessage += "</div></div>";
-$("#buttons").html(fieldMessage);
 
-$(".button").click(function() {
-	var buttonValue = $(this).attr("id");
-	var fieldSize = parseInt(buttonValue.slice(5,6)) + 2;
-	var randomTiles = [];
-	
-	buildArray(fieldSize,randomTiles);
-	console.log("randomTiles: " + randomTiles);
-	 
-	removeDups(fieldSize,randomTiles);
-	console.log("adjusted: " + randomTiles);
-	 
-	// for each row, randomly pick a values from randomTiles and display it
-	randomizeArray(fieldSize,randomTiles);
-	
-	// generate field based on button click
-	var tempCount = 0;
-	var field = "<div class='table'>";
-	for (var i = 0; i < fieldSize; i++) {
-	 	field += "<div class='row' id=fieldRow" + i + ">";
-	 	for (var j = 0; j < fieldSize; j++) {
-	 		field += "<div class='cell' id='fieldCell" + i + "-" + j + "'>" + randomTiles[tempCount] + "</div>";
-	 		tempCount++;
-	 	}
-	 	field += "</div>";
-	}
-	field += "</div>";
-	$("#field").html(field);	
-	// end field generation 
-});
+
+
 
 function buildArray(size,array) {
 	console.log("buildArray function: size: " + size);
@@ -218,4 +328,4 @@ function randomizeArray(size,array) {
 	console.log("original order: " + array);
 	console.log("final order: " + randomArray);
 }
-
+*/
