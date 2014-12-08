e298b409cf39740457cab1edef74585c971621ae
jawa9000 <jawa9000@gmail.com>
2014-12-08 13:11:37 -0800

    started porting my findings from 2dArray.html to this file.

--- js/memory.js
+++ js/memory.js
@@ -1,6 +1,16 @@
+//var defaultIcon = "question-sign";
+
+var idArray = []; // 1d array of all ids on game field elements
+var dataArray = []; // 1d array of all data values of game field elements
+var classArray = []; // 1d array of all classes of game field elements
+var id2DArray = []; // 2d array converted from 1d array of ids
+var class2DArray = []; // 2d array converted from 1d array of class
+var data2DArray = []; // 2d array converted from 1d array of data
+var iconTiles = []; // array to hold a list of icons to be display on the game field
 var icons = ["asterisk","cloud","music","heart","star","user","home","file","lock","headphones","volume-up","book","camera","picture","tint","play","arrow-right","fire","eye-open","thumbs-up","globe","phone","floppy-disk","tower","tree-deciduous"]; // all possible icons
-//var icons = ["cloud","cloud","heart","heart","star","star","home","home","lock","lock","volume-up","volume-up","camera","camera","tint","tint","fire","fire","eye-open","eye-open","globe","globe","floppy-disk","floppy-disk","tree-deciduous"]; // matching icons with one odd
-var defaultIcon = "question-sign";
+var iconNum = 0; // number of icons to generate
+var fieldSize = 0; // size of game field
+
 
 // Create buttons to select field size
 var fields = ["2x2","3x3","4x4","5x5","6x6"];
@@ -11,6 +21,143 @@ for (var i = 0; i < fields.length; i++) {
 fieldMessage += "</div></div>";
 $("#buttons").html(fieldMessage);
 
+// Display field based on button clicked
+$(".button").click(function() {
+	var fieldSize = $(this).attr("data"); // get value to be used for fieldSize
+	console.log("fieldSize: " + fieldSize);
+	iconNum = (fieldSize * fieldSize) / 2; // value for number of icons to create
+	console.log("iconNum: " + iconNum);
+
+	// start populating arrays	
+	iconTiles = []; // empty icon array	
+	if (iconNum % 2) { // if iconNum is odd, subject one and push the 'empty tile' icon into the array
+		iconNum--;
+		iconTiles.push("star-empty"); // this tile doesn't have a match
+	}
+	for (var i = 0; i < iconNum; i++) { // pick a pair of icons and push into array
+		var rndIcon = Math.floor(Math.random() * icons.length);
+		iconTiles.push(icons[rndIcon]);
+		iconTiles.push(icons[rndIcon]);
+	}
+	console.log("iconTiles array: " + iconTiles);
+	
+	// randomize order of iconTiles
+	var tempArray = []; // array to hold a copy of randomTiles
+	var randomArray = []; // array to hold randomly ordered version of randomTiles
+	for (i in iconTiles) { // copy randomTiles to tempArray
+	 	tempArray.push(iconTiles[i]);
+	}
+	for (var i = 0; i < iconTiles.length; i++) {
+		if (tempArray.length > 0) {
+			var rnd = Math.floor(Math.random() * (tempArray.length - 1));
+			randomArray.push(tempArray[rnd]);
+			tempArray.splice(rnd, 1);
+	 	}
+	}
+	console.log("original order (iconTiles): " + iconTiles);
+	console.log("final order (randomArray): " + randomArray);
+	
+	// create id array
+	for (var i = 0; i < fieldSize; i++) {
+		for (var j = 0; j < fieldSize; j++) {
+			idArray.push(i + "-" + j);
+		}
+	}
+	console.log("idArray: " + idArray);
+	
+	// create data array
+	// ** this may get merged with iconTiles....
+	for (i in iconTiles) {
+		dataArray.push(randomArray[i]);
+	}
+	console.log("dataArray: " + dataArray);
+	
+	// create class array
+	for (var i = 0; i < fieldSize; i++) {
+		for (var j = 0; j < fieldSize; j++) {
+			classArray.push("cell center-block glyphicon");
+		}
+	}
+	console.log("classArray: " + classArray);
+	
+	// set up various 1d arrays into 2d arrays
+	convertArray(idArray,id2DArray,fieldSize);
+	convertArray(dataArray,data2DArray,fieldSize);
+	convertArray(classArray,class2DArray,fieldSize);
+	
+	generateField(fieldSize,id2DArray,class2DArray,data2DArray);
+});
+
+// Utility functions
+function convertArray(array1d,array2d,fieldSize) {
+	//console.log("setting up " + array1d + " to become a 2d array");
+	console.log("executing converArray function. fieldSize = " + fieldSize);
+	var count = 0;
+	for (var i = 0; i < fieldSize; i++) {
+		var tempArray = [];
+		for (var j = 0; j < fieldSize; j++) {
+			tempArray.push(array1d[count]);
+			count++;
+		}
+		array2d.push(tempArray);
+	}
+	console.log(array2d);
+}
+
+function generateField(fieldSize,id2DArray,class2DArray,data2DArray) { // generate game field and display it
+	// update class2DArray
+	// ** note: there is an issue that if the select field size button is clicked before the game is finished, 
+	// ** the following will append to the class array and mess up the display of the game field. This should not be a problem
+	// ** as the select field size buttons will be hidden during game play.
+	for (var i = 0; i < fieldSize; i++) {
+		for (var j = 0; j < fieldSize; j++) {
+			if (fieldSize == 2) { // col-md-6
+				class2DArray[i][j] += " col-md-6 ";
+			} else if (fieldSize == 3) { //col-md-4
+				class2DArray[i][j] += " col-md-4 ";
+			} else if (fieldSize == 4) { //col-md-3
+				class2DArray[i][j] += " col-md-3 ";
+			} else if (fieldSize == 5 || fieldSize == 6) { //col-md-2
+				class2DArray[i][j] += " col-md-2 ";
+			}
+			if (data2DArray[i][j] == "star-empty") {
+				class2DArray[i][j] += " glyphicon-star-empty ";
+			} else {
+				class2DArray[i][j] += " glyphicon-question-sign ";
+			}
+		}
+	}
+	
+	//set up outer elements
+	var message = "<div class='row'>";
+	message += "<div class='col-md-2'></div>"; // left side
+	message += "<div class='col-md-8'>"; // start of center
+	for (var i = 0; i < fieldSize; i++) {
+		message += "<div class='row'>";
+			if (fieldSize == 5) {
+				message += "<div class='col-md-1'></div>";
+			}
+		for (var j = 0; j < fieldSize; j++) {
+			message += "<div id='" + id2DArray[i][j] + "' class='" + class2DArray[i][j] + "' data='" + data2DArray[i][j] + "'></div>";
+		}
+		if (fieldSize == 5) {
+			message += "<div class='col-md-1'></div>";
+		}
+		message += "</div>";
+	}
+	message += "</div>";
+	// finish up outer elements
+	message += "</div>"; // end of center
+	message += "<div class='col-md-2'></div></div>"; // right side and end of row
+	$("#field").html(message);
+	fieldSize = 0;
+	id2DArray = [];
+	class2DArray = [];
+	data2DArray = [];
+}
+
+
+/*
 // Display field based on button clicked
 $(".button").click(function() {
 	var buttonValue = $(this).attr("data");
@@ -60,8 +207,7 @@ $(".button").click(function() {
 	field += "</div>";
 	$("#field").html(field);
 });
-
-
+*/
 
 
 /*
