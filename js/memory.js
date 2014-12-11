526845afef04fbe596703041655201a5c4c79350
jawa9000 <jawa9000@gmail.com>
2014-12-11 13:09:41 -0800

    renamed several ids for better identification.

--- js/memory.js
+++ js/memory.js
@@ -12,60 +12,39 @@ var iconNum = 0; // number of icons to generate
 var fieldSize = 0; // size of game field
 var fields = ["2x2","3x3","4x4","5x5","6x6"]; // game field sizes
 var message = ""; // variable to hold game status message(s)
-// build menu options section
-// toggle menu options section based on where the game is currently
-	// start of game, display
-	// game started, hide; show menu table
-	// game won, display
+var count = 0; // generic number to iterate through icon array when drawing the board
+var clicks = 0; // count the number of times the user clicks
+var clicksArray = []; // store the clicked icon's data
+var clickedIdArray = []; // store the cliked icon's id
+var score = 0; // store score for current game
+var gameTally = 0; // store total score for all games played this session
+var matched = false; // boolean value if the icons match or not
 
-//var gameStatus = "start";
-checkGameStatus("start");
-function checkGameStatus(gameStatus) {
-	if (gameStatus == "start") { // show menu options
-		message = "Welcome to the Memory game<br/>To start the game, clicked on one of these buttons";
-		$("#message").html(message);
-		buildFieldButtons();
-	} else if (gameStatus == "active") { // hide menu options, show game field
-		$("#menu").slideUp("fast");
-		$("#gameTab").css("display","block").slideDown("fast");
-		// show menu options tab
-	} else if (gameStatus == "won") {
-		$("#gameTab").slideUp("fast");
-		// show menu options
-		buildFieldButtons();
-		// display "you've won" message
-		message = "You won!<br/>To play again, click on one of these buttons";
-		$("#message").html(message);
-	}
-	console.log("gameStatus: " + gameStatus);
-}
-$("#gameTab").click(function() {
-	$("#gameTab").slideUp("fast").css("display","none");
-	$("#menu").slideDown("fast");
-	// ** insert game field reset here!!! **
-});
+checkGameStatus("start"); // display game field selection options and hide the game selectMenu button
 
 // Display field based on button clicked
 $(".button").click(function() {
-	checkGameStatus("active"); // hide menu section
+	checkGameStatus("active"); // hide selectMenu section
 	fieldSize = $(this).attr("data"); // get value to be used for fieldSize
 	//console.log("fieldSize: " + fieldSize);
 	iconNum = (fieldSize * fieldSize) / 2; // value for number of icons to create
 	//console.log("iconNum: " + iconNum);
-
+	targetScore = Math.floor(fieldSize * fieldSize);
+	
 	// start populating arrays	
 	iconTiles = []; // empty icon array	
 	if (iconNum % 2) { // if iconNum is odd, subject one and push the 'empty tile' icon into the array
 		iconNum--;
 		iconTiles.push("star-empty"); // this tile doesn't have a match
+		targetScore--;
 	}
+	//console.log(targetScore);
 	for (var i = 0; i < iconNum; i++) { // pick a pair of icons and push into array
 		var rndIcon = Math.floor(Math.random() * icons.length);
 		iconTiles.push(icons[rndIcon]);
 		iconTiles.push(icons[rndIcon]);
 	}
 	//console.log("iconTiles array: " + iconTiles);
-	
 	// randomize order of iconTiles
 	var tempArray = []; // array to hold a copy of randomTiles
 	var randomArray = []; // array to hold randomly ordered version of randomTiles
@@ -100,11 +79,10 @@ $(".button").click(function() {
 	// create class array
 	for (var i = 0; i < fieldSize; i++) {
 		for (var j = 0; j < fieldSize; j++) {
-			classArray.push("cell center-block glyphicon");
+			classArray.push("gameCell center-block glyphicon");
 		}
 	}
 	//console.log("classArray: " + classArray);
-	
 	// set up various 1d arrays into 2d arrays
 	convertArray(idArray,id2DArray,fieldSize);
 	convertArray(dataArray,data2DArray,fieldSize);
@@ -113,30 +91,27 @@ $(".button").click(function() {
 	generateField(fieldSize,id2DArray,class2DArray,data2DArray);
 });
 
-var count = 0; // generic number to iterate through icon array when drawing the board
-//var boardSize = 5; // number of rows and columns to create for the board
-var clicks = 0; // count the number of times the user clicks
-var clicksArray = []; // store the clicked icon's data
-var clickedIdArray = []; // store the cliked icon's id
-var score = 0;
+// selectMenu interactions
+$("#inGameMenu").click(function() {
+	$("#inGameMenu").slideUp("fast").css("display","none");
+	$("#selectMenu").slideDown("fast");
+	// ** insert game field reset here!!! **
+});
+
 
 // game field interactions
-$(document).on("click",".cell",function() {
-	console.log("on icon click, fieldSize is: " + fieldSize);
+$(document).on("click",".gameCell",function() {
+	//console.log("on icon click, fieldSize is: " + fieldSize);
 	var clickedId = $(this).attr("id");
-	console.log("clickedId: " + clickedId);
+	//console.log("clickedId: " + clickedId);
 	var clickedData = $(this).attr("data");
-	console.log("clickedData: " + clickedData);
+	//console.log("clickedData: " + clickedData);
 	var clickedClass = $(this).attr("class");
-	console.log("clickedClass: " + clickedClass);
+	//console.log("clickedClass: " + clickedClass);
 	
 	clickedIdArray.push(clickedId);
-	console.log("clickedIdArray: " + clickedIdArray);
-	// ** how many times has this been clicked? reset class and display when?
-	// ** push clickedData into a temp array
-	// ** if temp array[0] == [1], it's a match; remove icons from game play; tally score
-	// ** if score == target, game over; you win
-	if ($(this).hasClass("glyphicon-question-sign")) {
+	//console.log("clickedIdArray: " + clickedIdArray);
+	if ($(this).hasClass("gameCell")) {
 		var first = parseInt(clickedId.substr(0,1)); // with id, find value for first place in data2DArray
 		var second = parseInt(clickedId.substr(2,3)); // with id, find value for second place in data2Darray
 		// reset for column size
@@ -150,77 +125,85 @@ $(document).on("click",".cell",function() {
 		} else if (fieldSize == 5 || fieldSize == 6) { //col-md-2
 			tempClassInsert = " col-md-2 ";
 		}
-		// replace class array data with data2DArray value and update col-md class
-		class2DArray[first][second] = "cell center-block glyphicon " + tempClassInsert + " glyphicon-" + data2DArray[first][second];
-		//console.log(fieldSize);
-		
+		class2DArray[first][second] = "gameCell center-block glyphicon " + tempClassInsert + " glyphicon-" + data2DArray[first][second]; // replace class array data with data2DArray value and update col-md class
+		//console.log(fieldSize);		
 		redrawGameField(fieldSize,id2DArray,class2DArray,data2DArray);// redraw game field
 	}
 	clicks++;
 	console.log("clicks: " + clicks);
 	if (clicks == 2) {
 		clicksArray.push(clickedData);
-		console.log(clicksArray);
-		console.log(clickedIdArray[0][0] + "," + clickedIdArray[0][2]);
-		console.log(clickedIdArray[1][0] + "," + clickedIdArray[1][2]);
-		if (clicksArray[0] == clicksArray[1]) {
+		console.log("clickedIdArray: " + clickedIdArray);
+		//console.log(clickedIdArray[0][0] + "," + clickedIdArray[0][2]);
+		//console.log(clickedIdArray[1][0] + "," + clickedIdArray[1][2]);
+		if (clicksArray[0] == clicksArray[1] && clickedIdArray[0] != clickedIdArray[1]) { // clicked icons match and the same icon isn't clicked twice
+			matched = true;
 			console.log(clicksArray[0] + " = " + clicksArray[1]);
 			// remove icon from game field
-			class2DArray[parseInt(clickedIdArray[0][0])][parseInt(clickedIdArray[0][2])] = "noDisplay center-block glyphicon " + tempClassInsert + " glyphicon-" + data2DArray[parseInt(clickedIdArray[0][0])][parseInt(clickedIdArray[0][2])]; // replace cell with noDisplay class in class2DArray
-			class2DArray[parseInt(clickedIdArray[1][0])][parseInt(clickedIdArray[1][2])] = "noDisplay center-block glyphicon " + tempClassInsert + " glyphicon-" + data2DArray[parseInt(clickedIdArray[1][0])][parseInt(clickedIdArray[1][2])]; // replace cell with noDisplay class in class2DArray
-			//console.log(class2DArray);
+			class2DArray[parseInt(clickedIdArray[0][0])][parseInt(clickedIdArray[0][2])] = "noDisplay center-block glyphicon " + tempClassInsert + " glyphicon-" + data2DArray[parseInt(clickedIdArray[0][0])][parseInt(clickedIdArray[0][2])]; // replace gameCell with noDisplay class in class2DArray
+			class2DArray[parseInt(clickedIdArray[1][0])][parseInt(clickedIdArray[1][2])] = "noDisplay center-block glyphicon " + tempClassInsert + " glyphicon-" + data2DArray[parseInt(clickedIdArray[1][0])][parseInt(clickedIdArray[1][2])]; // replace gameCell with noDisplay class in class2DArray
+			console.log(class2DArray);
 			redrawGameField(fieldSize,id2DArray,class2DArray,data2DArray);// redraw game field
+			console.log(class2DArray);
 			score += 2;
+			$("#score").html(score);
+			console.log(score);
 			// check if all icons have been matched
+			if (score == targetScore) {
+				console.log("You've won the game!");
+				gameTally += score;
+				// show game selectMenu; hide game field
+				$("#tally").html(gameTally);
+			}
 		} else {
 			console.log(clicksArray[0] + " != " + clicksArray[1]);
-			
-			// not a match, reset icon display
-			// redisplay game field
+			matched = false;
 		}
 	} else if (clicks == 3) {
-		class2DArray[parseInt(clickedIdArray[0][0])][parseInt(clickedIdArray[0][2])] = "cell center-block glyphicon " + tempClassInsert + " glyphicon-question-sign"; // reset class2DArray
-		class2DArray[parseInt(clickedIdArray[1][0])][parseInt(clickedIdArray[1][2])] = "cell center-block glyphicon " + tempClassInsert + " glyphicon-question-sign"; // reset class2DArray
-		class2DArray[first][second] = "cell center-block glyphicon " + tempClassInsert + " glyphicon-" + data2DArray[first][second];
+		//console.log(class2DArray);
+		if (!matched) {
+			class2DArray[parseInt(clickedIdArray[0][0])][parseInt(clickedIdArray[0][2])] = "gameCell center-block glyphicon " + tempClassInsert + " glyphicon-question-sign"; // reset class2DArray
+			class2DArray[parseInt(clickedIdArray[1][0])][parseInt(clickedIdArray[1][2])] = "gameCell center-block glyphicon " + tempClassInsert + " glyphicon-question-sign"; // reset class2DArray
+			matched = false;
+		}
+		//console.log(class2DArray);
+		class2DArray[first][second] = "gameCell center-block glyphicon " + tempClassInsert + " glyphicon-" + data2DArray[first][second];		
 		redrawGameField(fieldSize,id2DArray,class2DArray,data2DArray);// redraw game field
 		// resets
 		clicksArray = [];
 		clickedIdArray = [];
 		clickedIdArray.push(clickedId);
 		clicksArray.push(clickedData);
-		
 		clicks = 1;
 	} else {
 		clicksArray.push(clickedData);
 	}
-	/*
-	if (clicksArray.length == 2) { // if the user has clicked twice
-		console.log("third click, hide all");
-		//$(".cell").removeClass("glyphicon-" + clicksArray[0]).removeClass("glyphicon").removeClass("cell"); // remove all classes
-		//$(".cell").removeClass("glyphicon-" + clicksArray[1]).removeClass("glyphicon").removeClass("cell"); // remove all classes
-		//for (var i = 0; i < boardSize; i++) { // reset classes
-		//	for (var j = 0; j < boardSize; j++) {
-		//		$("#cell-" + i + "-" + j).attr("class","cell glyphicon glyphicon-" + defaultIcon); // this forces the order of the classes to the proper order.
-		//	}
-		//}
-		clicks = 0;
-		clicksArray = []; // empty clicks array
-		clicksArray.push(iconData); // populate clicks array with current click
-		console.log(clicksArray);
-	} else { // user hasn't clicked twice yet
-		clicksArray.push(iconData);
-		console.log("clicksArray: " + clicksArray);
-	}
-	*/
-	// get class
-	// find  glyphicon-question-sign and replace it with  glyphicon-question- + data
-	// store this in a temp array
-	// if temp array[0] == [1], match found, remove icons from game play, add to score
 });
 
 // Utility functions
+function checkGameStatus(gameStatus) {
+	if (gameStatus == "start") { // show selectMenu options
+		message = "Welcome to the Memory game<br/>To start the game, clicked on one of these buttons";
+		$("#message").html(message);
+		buildFieldButtons();
+	} else if (gameStatus == "active") { // hide selectMenu options, show game field
+		//$("#selectMenu").slideUp("fast").delay(10500);
+		$("#selectMenu").slideUp("fast");
+		$("#inGameMenu").css("display","block").slideDown("fast");
+		// show selectMenu options tab
+	} else if (gameStatus == "won") {
+		$("#inGameMenu").slideUp("fast");
+		// show selectMenu options
+		buildFieldButtons();
+		// display "you've won" message
+		message = "You won!<br/>To play again, click on one of these buttons";
+		$("#message").html(message);
+	}
+	//console.log("gameStatus: " + gameStatus);
+}
+
 function buildFieldButtons() { // Create buttons to select field size
-	var fieldMessage = "<div class='table'><div class='row'>";
+	var fieldMessage = "<div class='gameTable'><div class='gameRow'>";
 	for (var i = 0; i < fields.length; i++) {
 		fieldMessage += "<div data='" + (i+2) + "' class='button' id='field" + i + "'>" + fields[i] + "</div>";
 	}
@@ -229,11 +212,11 @@ function buildFieldButtons() { // Create buttons to select field size
 }
 
 function redrawGameField(fieldSize,id2DArray,class2DArray,data2DArray) {
-	var message = "<div class='row'>";
+	var message = "<div class='gameRow'>";
 	message += "<div class='col-md-2'></div>"; // left side
 	message += "<div class='col-md-8'>"; // start of center
 	for (var i = 0; i < fieldSize; i++) {
-	message += "<div class='row'>";
+	message += "<div class='gameRow'>";
 		if (fieldSize == 5) {
 			message += "<div class='col-md-1'></div>";
 		}
@@ -302,13 +285,6 @@ function generateField(fieldSize,id2DArray,class2DArray,data2DArray) { // genera
 
 
 /*
-var count = 0; // generic number to iterate through icon array when drawing the board
-var boardSize = 5; // number of rows and columns to create for the board
-var clicks = 0; // count the number of times the user clicks
-var clicksArray = []; // store the clicked icon's data
-
-
-
 $(document).ready(function() {
 	$(".cell").click(function() {
 		var iconData = $(this).attr("data"); // get data
@@ -345,129 +321,4 @@ $(document).ready(function() {
 		console.log(iconData);
 	});
 });
-
-// fields and ramdonly picked cell values
-
-
-
-
-
-function buildArray(size,array) {
-	console.log("buildArray function: size: " + size);
-	console.log("buildArray function: array: " + array);
-	for (var i = 0; i < size * size; i++) {
-		var ranNum = Math.floor(Math.random() * 20); // there are up to 6 different icons to choose from
-		array.push(ranNum);
-	}
-}
-function removeDups(size,array) {
- 	for (var i = 0; i < size; i++) {
-	 	var a = i;
-	 	var b = a + 1;
-	 	if (b > size - 1) {
-	 		b = b - size;
-	 	}
-	 	var c = b + 1;
-	 	if (c > size - 1) {
-	 		c = c - size;
-	 	}
-	 	var d = c + 1;
-	 	if (d > size - 1) {
-	 		d = d - size;
-	 	}
-	 	var e = d + 1;
-	 	if (e > size - 1) {
-	 		e = e - size;
-	 	}
-	 	var f = e + 1;
-	 	if (f > size - 1) {
-	 		f = f - size;
-	 	}
-	 	
-	 	//console.log("a:" + a + " b:" + b + " c:" + c + " d:" + d + " e:" + e);
-		switch (size) {
-		 	case 2:
-			 	if (array[a] == array[b]) {
-			 		while(array[a] == array[b]) {
-			 			if (array[a] + 1 < 20) {
-			 				array[a]++;
-			 			}
-			 		}
-			 	}
-		 		break;
-		 	case 3:
-			 	if (array[a] == array[b] || array[a] == array[c]) {
-			 		while(array[a] == array[b] || array[a] == array[c]) {
-			 			if (array[a] + 1 < 20) {
-			 				array[a]++;
-			 			}
-			 		}
-			 	}
-		 		break;
-		 	case 4:
-		 		if (array[a] == array[b] || array[a] == array[c] || array[a] == array[d]) {
-			 		while(array[a] == array[b] || array[a] == array[c] || array[a] == array[d]) {
-			 			if (array[a] + 1 < 20) {
-			 				array[a]++;
-			 			}
-			 		}
-			 	}
-		 		break;
-		 	case 5:
-		 		if (array[a] == array[b] || array[a] == array[c] || array[a] == array[d] || array[a] == array[e]) {
-			 		while(array[a] == array[b] || array[a] == array[c] || array[a] == array[d] || array[a] == array[e]) {
-			 			if (array[a] + 1 < 20) {
-			 				array[a]++;
-			 			}
-			 		}
-			 	}
-		 		break;
-		 	case 6:
-		 		if (array[a] == array[b] || array[a] == array[c] || array[a] == array[d] || array[a] == array[e] || array[a] == array[f]) {
-			 		while(array[a] == array[b] || array[a] == array[c] || array[a] == array[d] || array[a] == array[e] || array[a] == array[f]) {
-			 			if (array[a] + 1 < 20) {
-			 				array[a]++;
-			 			}
-			 		}
-			 	}
-		 		break;
-		 }
- 	}
- 	
- 	// ** needs a step to take the first half of the array, and duplicate it so the field can have matching items **
- 	console.log("array before the cut: " + array);
- 	var tempArray = [];
- 	for (var i = 0; i < (array.length / 2); i++) {
-		tempArray.push(array[i]);
-		tempArray.push(array[i]);
- 	}
- 	tempArray.pop();
- 	console.log("array with single dups: " + tempArray);
- 	console.log("array length: " + tempArray.length);
- 	
- 	array = [];
- 	for (i in tempArray) {
- 		array.push(tempArray[i]);
- 	}
- 	
- 	// ** this works for going through the array and making a set of matching items. However, the rest of this function prohibits duplicates from happening.
- 	// ** adjust this script
-}
-	 
-function randomizeArray(size,array) {
-	var tempArray = []; // array to hold a copy of randomTiles
-	var randomArray = []; // array to hold randomly ordered version of randomTiles
-	for (i in array) { // copy randomTiles to tempArray
-	 	tempArray.push(array[i]);
-	}
-	for (var i = 0; i < size; i++) {
-		if (tempArray.length > 0) {
-			var rnd = Math.floor(Math.random() * (tempArray.length - 1));
-			randomArray.push(tempArray[rnd]);
-			tempArray.splice(rnd, 1);
-	 	}
-	}
-	console.log("original order: " + array);
-	console.log("final order: " + randomArray);
-}
 */
